f06128880c5d62299e5f94ee3c28a81a7a5bfa47
martijnbentum <bentummartijn@gmail.com>
2026-01-08 14:53:34 +0100

    optional overwrite; added optional text entry - made text_filename optional; added a final report when all files are processed

--- webmaus/pipeline.py
+++ webmaus/pipeline.py
@@ -9,7 +9,7 @@ from .connector import run_pipeline, make_output_filename
 class Pipeline:
     def __init__(self, files, output_directory, language, 
         output_format = 'TextGrid', pipe = 'G2P_MAUS_PHO2SYL',
-        preseg = 'true', language_dict = None):
+        preseg = 'true', language_dict = None, overwrite = False):
         '''Initialize the Pipeline object to handle forced alignment of
         orthographically annotated speech recordings.
         files:              list of dicts with 'audio_filename' and 
@@ -30,6 +30,7 @@ class Pipeline:
         self.pipe = pipe
         self.preseg = preseg
         self.language_dict = language_dict
+        self.overwrite = overwrite
 
         self.done = []
         self.skipped = []
@@ -47,28 +48,51 @@ class Pipeline:
     def run(self):
         for entry in progressbar(self.files):
             audio_filename = entry['audio_filename']
-            text_filename = entry['text_filename']
+            text_filename = entry.get('text_filename', None)
             start_time = entry.get('start_time', None)
             end_time = entry.get('end_time', None)
+            text = entry.get('text', None)
 
             output_file = make_output_filename(self.output_directory,
                 audio_filename, self.output_format, start_time, end_time)
 
-            if Path(output_file).exists():
-                self.skipped.append(audio_filename)
+            if Path(output_file).exists() and not self.overwrite:
+                self.skipped.append((audio_filename, start_time, end_time))
                 continue
 
             ok = self._throttle()
-            if not ok: break
+            if not ok: 
+                print("Work interrupted due to thread pool restart.")
+                break
 
             thread = threading.Thread(target=self._run_single,
-                args=(audio_filename, text_filename, start_time, end_time))
+                args=(audio_filename, text_filename, start_time, end_time,
+                    text))
             thread.start()
             self.executors.append(thread)
-
             time.sleep(self.wait_time)
+            if len(self.executors) > 0:
+                print(f'n executors: {len(self.executors)}')
+        print("Waiting for all threads to complete...")
+        while len(self.executors) > 0:
+            time.sleep(self.wait_time)
+            ok = self._throttle()
+            if not ok:
+                print("Work interrupted due to thread pool restart.")
+                break
+        print("All audio files processed.")
+        m = f'Done: {len(self.done)}, '
+        m += f'Skipped: {len(self.skipped)}, '
+        m += f'Errors: {len(self.errors)}'
+        m += f'\nFiles can be found in : {self.output_directory}'
+        print(m)
 
-    def _run_single(self, audio_filename, text_filename, start_time, end_time):
+    def _run_single(self, audio_filename, text_filename, start_time = None, 
+        end_time = None, text=None):
+        '''Run the forced alignment pipeline for a single audio-text pair.
+        audio_filename:     path to the audio file
+        text_filename:      path to the text file
+        '''
         language = self.language
 
         if self.language_dict:
@@ -84,6 +108,7 @@ class Pipeline:
             output_format=self.output_format,
             pipe=self.pipe,
             preseg=self.preseg,
+            text=text,
         )
 
         if response is None:
@@ -93,10 +118,19 @@ class Pipeline:
             self.errors.append(audio_filename)
             return
 
-        response.save_alignment(output_directory = self.output_directory)
+        response.save_alignment(output_directory = self.output_directory,
+            audio_filename = audio_filename, start_time = start_time,
+            end_time = end_time)
         self.done.append([audio_filename, response.output_filename])
 
     def _throttle(self):
+        '''Throttle the number of concurrent threads to avoid overloading 
+        the system.
+        checks the number of active threads every second, and if there are
+        more than 6 active threads for more than 20 minutes, it restarts
+        (assuming some threads are stuck).
+        cleans ups finished threads from the executor list.
+        '''
         self.executors = [e for e in self.executors if e.is_alive()]
         self.start = time.time()
         do_restart = False
